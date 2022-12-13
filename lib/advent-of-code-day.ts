ef88cb0796e43eca4cb54541b5c7cfb8190f7c25
Kyle Laker <kyle@laker.email>
2022-12-12 23:45:51 -0500

    Support higher memory and timeout

--- lib/advent-of-code-day.ts
+++ lib/advent-of-code-day.ts
@@ -1,3 +1,4 @@
+import * as cdk from 'aws-cdk-lib';
 import * as apigw from '@aws-cdk/aws-apigatewayv2-alpha';
 import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
@@ -8,16 +9,23 @@ export interface AdventOfCodeDayProps {
   readonly day: number;
   readonly name: string;
   readonly api: apigw.HttpApi;
+  readonly timeout?: cdk.Duration;
+  readonly memory?: number;
 }
 
 export class AdventOfCodeDay extends Construct {
   constructor(scope: Construct, id: string, props: AdventOfCodeDayProps) {
     super(scope, id);
     const codePath = `lambda/day/${props.day}/solution.ts`;
+    if ((props.timeout?.toSeconds() ?? 3) > 30) {
+      throw new Error('API integration timeout is limited to 30 seconds');
+    }
     const fn = new nodejs.NodejsFunction(this, `SolutionDay${props.day}`, {
       entry: codePath,
       runtime: lambda.Runtime.NODEJS_18_X,
       tracing: lambda.Tracing.ACTIVE,
+      timeout: props.timeout,
+      memorySize: props.memory ?? 256,
     });
     props.api.addRoutes({
       methods: [apigw.HttpMethod.POST],
